b9202f20f9eada0843997e463672c5b36c4e7d7c
Iaven00 <1245325661@qq.com>
2022-07-07 01:43:22 +0000

    表格结构更新

---
+++ Monitor/src/main/java/bjtu/monitor/mapper/AdministratorMapper.java
@@ -0,0 +1,30 @@
+package bjtu.monitor.mapper;
+
+import bjtu.monitor.pojo.table.Administrator;
+import bjtu.monitor.pojo.table.AdministratorExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface AdministratorMapper {
+    int countByExample(AdministratorExample example);
+
+    int deleteByExample(AdministratorExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Administrator record);
+
+    int insertSelective(Administrator record);
+
+    List<Administrator> selectByExample(AdministratorExample example);
+
+    Administrator selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Administrator record, @Param("example") AdministratorExample example);
+
+    int updateByExample(@Param("record") Administrator record, @Param("example") AdministratorExample example);
+
+    int updateByPrimaryKeySelective(Administrator record);
+
+    int updateByPrimaryKey(Administrator record);
+}
