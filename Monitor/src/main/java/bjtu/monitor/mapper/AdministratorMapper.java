6001d2cd9327c1c5f7be72d301e01c3accd8a760
Iaven00 <1245325661@qq.com>
2022-07-07 03:30:33 +0000

    update Monitor/src/main/java/bjtu/monitor/mapper/AdministratorMapper.java.

--- Monitor/src/main/java/bjtu/monitor/mapper/AdministratorMapper.java
+++ Monitor/src/main/java/bjtu/monitor/mapper/AdministratorMapper.java
@@ -5,6 +5,8 @@ import bjtu.monitor.pojo.table.AdministratorExample;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
+@Mapper
+@Repository
 public interface AdministratorMapper {
     int countByExample(AdministratorExample example);
 
