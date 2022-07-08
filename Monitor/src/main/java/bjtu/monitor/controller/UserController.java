900a8a2ca8745aee0ca4f9c2c266551e1594f09a
Iaven00 <1245325661@qq.com>
2022-07-08 02:00:53 +0000

    update Monitor/src/main/java/bjtu/monitor/controller/UserController.java.

--- Monitor/src/main/java/bjtu/monitor/controller/UserController.java
+++ Monitor/src/main/java/bjtu/monitor/controller/UserController.java
@@ -1,54 +1,46 @@
 package bjtu.monitor.controller;
+
 import bjtu.monitor.pojo.table.User;
-import bjtu.monitor.utils.Global;
-import bjtu.monitor.utils.ReturnObject;
-import com.sun.mail.imap.Rights;
+import bjtu.monitor.service.MailService;
+import bjtu.monitor.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.List;
-import bjtu.monitor.service.UserService;
+import java.util.Date;
 
-import static bjtu.monitor.utils.Global.SUCCESS;
+import static bjtu.monitor.utils.Global.*;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-
     @Autowired
-    private UserService userService;
-//    @Autowired
-//    private RightsService rightsService;
-
-
-
-    /*
-     * 登录请求，登录成功跳转到首页;失败重定向(redirect:xxx)到登录页面，并给出相应提示
-     * */
+    UserService userService;
+    @Autowired
+    MailService mailService;
     @GetMapping("/login/{username}/{password}")
     @ResponseBody
     public int loginUser(@PathVariable("username")String username,
                           @PathVariable("password")String password,
                           HttpSession session, HttpServletResponse response){
-
-
-        User user = userService.loginUser(username,password);
-        if(user==null){
-            return 0;
-        }else {
-//            List<Rights> rights = rightsService.listRights(user.getUserid());
-//            user.setUserRights(rights);
-            session.setAttribute("nowUser", user);
-            Cookie cookie = new Cookie("id", String.valueOf(user.getUserid()));
-            cookie.setPath("/");
-            response.addCookie(cookie);
-            System.out.println("登录 Cookie saved");
-        }
         return 200;
+    }
+    @PostMapping("/register")
+    @ResponseBody
+    public int register(User user, @RequestParam("code")String codeValue){
+        String code = mailService.getCode(user.getEmail(),REGISTER);
+        if(codeValue.compareTo(code)!=0){
+            return CODEERROR;
+        }
+        user.setSex("男");
+        user.setRegistertime(new Date());
+        user.setRights(100);
+        if(userService.registerUser(user)!=null){
+            return SUCCESS;
+        }
+        return FAIL;
     }
 
 
