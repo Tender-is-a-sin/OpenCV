0c0d66e58a590aa19e9cd8d30e8840677293dcb9
asuak <10226854+asuak@user.noreply.gitee.com>
2022-07-08 08:05:26 +0000

    update Monitor/src/main/java/bjtu/monitor/config/openapiMainApplication.java.

--- Monitor/src/main/java/bjtu/monitor/config/openapiMainApplication.java
+++ Monitor/src/main/java/bjtu/monitor/config/openapiMainApplication.java
@@ -1,6 +1,8 @@
 package bjtu.monitor.config;
 
 import java.io.UnsupportedEncodingException;
+
+import static bjtu.monitor.utils.Global.*;
 //import org.dom4j.*;
 
 
@@ -8,13 +10,13 @@ public class openapiMainApplication {
     public static void main(String[] args) throws UnsupportedEncodingException {
         //此为opencv的opencv_java453.dll
         //位置在opencv安装目录下的build\\java\\x64\\位置
-        String dllAbsPath = "D:\\Coding\\java\\git\\video-detection-system\\Monitor\\src\\main\\resources\\lib.opencv\\opencv_java460.dll";
-        //位置在opencv安装目录下的sources\\data\\haarcascades\\位置
-        String facexmlAbsPath = "D:\\java\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_alt.xml";
+//        String dllAbsPath = "D:\\Coding\\java\\git\\video-detection-system\\Monitor\\src\\main\\resources\\lib.opencv\\opencv_java460.dll";
+//        //位置在opencv安装目录下的sources\\data\\haarcascades\\位置
+//        String facexmlAbsPath = "D:\\java\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_alt.xml";
         //必须加载
         String eyexmlAbsPath="0";
         InitInstance.init(dllAbsPath, facexmlAbsPath,eyexmlAbsPath);
-               InitInstance.videoDetectorModel();
-//              System.out.println(InitInstance.compare_image("C:\\Users\\86137\\Desktop\\test.jpg", "C:\\Users\\86137\\Desktop\\test.jpg"));
+        InitInstance.videoDetectorModel();
+
     }
 }
