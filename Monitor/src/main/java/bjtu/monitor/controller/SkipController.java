e491098fd1d7c3935a204fd7bbdb0a1b19aa9a24
张钰杰 <1174949581@qq.com>
2022-07-09 02:26:39 +0000

    update Monitor/src/main/java/bjtu/monitor/controller/SkipController.java.

--- Monitor/src/main/java/bjtu/monitor/controller/SkipController.java
+++ Monitor/src/main/java/bjtu/monitor/controller/SkipController.java
@@ -15,15 +15,8 @@ public class SkipController {
         return "html/Register";
     }
 
-    @GetMapping("/toHome")
-    public String toHome(){
-        return "html/home";
-    }
 
-    @GetMapping("/toObfuscation")
-    public String toObfuscation(){
-        return "html/obfuscation";
-    }
+
 
     @GetMapping("/toRecord")
     public String toRecord(){
@@ -35,39 +28,79 @@ public class SkipController {
         return "html/replay";
     }
 
-    @GetMapping("/toFacesign")
-    public String toFacesign(){
-        return "html/admin/facesign";
-    }
-
-    @GetMapping("/toRealtimemonitor")
-    public String toRealtimemonitor(){
-        return "html/admin/realtimemonitor";
-    }
-    @GetMapping("/toUser_info_jurisdiction")
-    public String toUser_info_jurisdiction(){
-        return "html/admin/user_info_jurisdiction";
+    @GetMapping("/toFace")
+    public String toFace(){
+        return "html/admin/face";
     }
     @GetMapping("/toAdmin")
-    public String toAdmin(){
-        return "html/admin/statistics";
+    public String toHome_admin(){
+        return "html/admin/home_admin";
     }
 
-    @GetMapping("/toStatistics")
-    public String toStatistics(){
-        return "html/admin/statistics";
+    @GetMapping("/toMonitor_admin")
+    public String toMonitor_admin(){
+        return "html/admin/monitor_admin";
     }
-
     @GetMapping("/toRecord_admin")
     public String toRecord_admin(){
         return "html/admin/record_admin";
     }
+
     @GetMapping("/toReplay_admin")
     public String toReplay_admin(){
         return "html/admin/replay_admin";
     }
+
+    @GetMapping("/to_User_info_jurisdiction")
+    public String toUser_info_jurisdiction(){
+        return "html/admin/user_info_jurisdiction";
+    }
+
+    @GetMapping("/toStatic_admin")
+    public String toStatic_admin(){
+        return "html/admin/static_admin";
+    }
+
+
+
     @GetMapping("/toWhitelist")
     public String toWhitelist(){
         return "html/admin/whitelist";
+    }
+    @GetMapping("/toRecord_mem")
+    public String toRecord_mem(){
+        return "html/member/record_mem";
+    }
+    @GetMapping("/toStatistics_mem")
+    public String toStatistics_mem(){
+        return "html/member/static_mem";
+    }
+    @GetMapping("/toMonitor")
+    public String toMonitor(){
+        return "html/member/monitor";
+    }
+    @GetMapping("/toReplay_mem")
+    public String toReplay_mem(){
+        return "html/member/replay_mem";
+    }
+    @GetMapping("/toEdit")
+    public String toEdit(){
+        return "html/member/edit";
+    }
+    @GetMapping("/toResetpwd")
+    public String toResetpwd(){
+        return "html/member/resetpwd";
+    }
+    @GetMapping("/toStatic_mem")
+    public String toStatic_men(){
+        return "html/member/static_mem";
+    }
+    @GetMapping("/toHome_mem")
+    public String toHome_mem(){
+        return "html/member/home_mem";
+    }
+    @GetMapping("/toCancellation")
+    public String toCancellation(){
+        return "html/member/cancellation";
     }
 }
