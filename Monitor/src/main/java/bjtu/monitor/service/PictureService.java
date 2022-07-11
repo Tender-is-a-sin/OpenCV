582dfbe879186d277934d53ccf52003f9684e5e2
Iaven00 <1245325661@qq.com>
2022-07-11 01:13:50 +0000

    图片服务

---
+++ Monitor/src/main/java/bjtu/monitor/service/PictureService.java
@@ -0,0 +1,12 @@
+package bjtu.monitor.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.opencv.core.Mat;
+
+import java.io.IOException;
+
+public interface PictureService {
+    void getPicture(HttpServletRequest request, HttpServletResponse response,String p) throws IOException;
+
+}
