6c870cfdf2b3421cccf3b95692ca4e52e3fb2bb3
张钰杰 <1174949581@qq.com>
2022-07-13 08:13:06 +0000

    update Monitor/src/main/java/bjtu/monitor/controller/PictureController.java.

--- Monitor/src/main/java/bjtu/monitor/controller/PictureController.java
+++ Monitor/src/main/java/bjtu/monitor/controller/PictureController.java
@@ -32,6 +32,30 @@ public class PictureController {
             e.printStackTrace();
         }
     }
+
+
+     @GetMapping("/getPicture")
+    @ResponseBody
+    public String[] getPicture(String date) {
+        //  static\\intrusion填绝对路径
+        String paths = "F:\\video-detection-system-master (2)\\video-detection-system-master\\Monitor\\src\\main\\resources\\static\\intrusion";
+        String result[]=new String[24];
+        // 获得指定文件对象
+        File file = new File(paths);
+        // 获得该文件夹内的所有文件
+        File[] array = file.listFiles();
+
+        int j=0;
+        for(int i=0;i<array.length;i++)
+        {
+            if(array[i].getName().contains(date.toString())){
+                result[j]="/intrusion/"+array[j].getName();
+                System.out.println(result[j]);
+                j++;
+            }
+        }
+        return result;
+    }
 //    public void getBase64(HttpServletRequest request, HttpServletResponse response) {
 //        //调用doGet方法会抛出异常,捕获一下
 //        try {
