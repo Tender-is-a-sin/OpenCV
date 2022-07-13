085568dff0255369b65e111c87ce8a515fde6ca7
Iaven00 <1245325661@qq.com>
2022-07-13 12:39:34 +0000

    update Monitor/src/main/java/bjtu/monitor/controller/SseEmitterController.java.

--- Monitor/src/main/java/bjtu/monitor/controller/SseEmitterController.java
+++ Monitor/src/main/java/bjtu/monitor/controller/SseEmitterController.java
@@ -1,74 +1,160 @@
-package bjtu.monitor.controller;
-
-import bjtu.monitor.service.MonggoDB;
-import bjtu.monitor.utils.InitInstance;
-import bjtu.monitor.utils.SseEmitterServer;
-import bjtu.monitor.utils.savePicture;
-import org.opencv.core.*;
+package bjtu.monitor.utils;
 import org.opencv.imgcodecs.Imgcodecs;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.opencv.videoio.*;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.slf4j.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Objects;
 
 import static bjtu.monitor.utils.Global.*;
+import static org.opencv.imgproc.Imgproc.INTER_LINEAR;
+import static org.opencv.videoio.Videoio.CAP_FFMPEG;
 
+public class InitInstance {
 
-@RestController
-@RequestMapping("/sse")
-public class SseEmitterController {
+    private static Logger logger = LoggerFactory.getLogger(InitInstance.class);
+    //脸部识别实例
+    private static CascadeClassifier faceDetector;
+    //打开摄像头
+    VideoCapture videoCapture;
 
-    @Autowired
-    MonggoDB monggoDB;
-    InitInstance initInstance;
+    //此类加载人脸识别模块
+    public static void init(String dllAbsPath, String facexmlAbsPath, String eyexmlAbsPath) {
+        logger.info("开始读取脸部识别实例");
+        //加载dll文件
+        System.load(dllAbsPath);
+        faceDetector = new CascadeClassifier(facexmlAbsPath);
+        if (faceDetector.empty()) {
+            logger.error("人脸识别模块读取失败");
+        } else logger.info("人脸识别模块读取成功");
+    }
+    public  InitInstance() {
+        logger.info("开始读取脸部识别实例");
+        //加载dll文件
+        System.load(dllAbsPath);
+        faceDetector = new CascadeClassifier(facexmlAbsPath);
+        if (faceDetector.empty()) {
+            logger.error("人脸识别模块读取失败");
+        } else logger.info("人脸识别模块读取成功");
+    }
 
-    Thread t;
-
-    @GetMapping("/push/{message}")
-    public ResponseEntity<String> push(@PathVariable(name = "message") String message) {
-        for (int i = 0; i < 10; i++) {
-            SseEmitterServer.batchSendMessage(message + i);
+    public boolean openVideo(){
+        //打开摄像头
+//        String p = "D:\\java\\opencv\\build\\bin\\opencv_videoio_ffmpeg460_64.dll";
+//        System.load(p);
+//        String url = "rtmp://124.70.0.166:1935/rtmplive";
+//        videoCapture = new VideoCapture(url,CAP_FFMPEG);
+        videoCapture = new VideoCapture(0);
+        //判断摄像头是否打开
+        if (!videoCapture.open(0)) {
+            logger.error("相机打开失败");
+            return false;
         }
-//        SseEmitterServer.batchSendMessage(message);
-        return ResponseEntity.ok("WebSocket 推送消息给所有人");
+        return true;
     }
-
-    @GetMapping("/close/{userId}")
-    public void close(@PathVariable String userId){
-        SseEmitterServer.removeUser(userId);
-        initInstance.closeVideo();
-        t.isInterrupted();
-
+    public void closeVideo(){
+        videoCapture.release();
     }
-        @GetMapping("/connect/{userId}/{Id}")
-    public SseEmitter connect(@PathVariable String userId,@PathVariable int Id) {
-        SseEmitter  s = SseEmitterServer.connect(userId);
-        System.out.println("---------------"+Id+"----------------------");
-        t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                initInstance = new InitInstance();
-                if(initInstance.openVideo()){
-                    while (true){
-                        Mat img = initInstance.getMatfromVideo();
-                        String tem = initInstance.matToBase64(img);
-                        String path = savepath+savePicture.getTime();
-                        Imgcodecs.imwrite(path,img);
-                        monggoDB.uploadFile(Id,path);
-                        SseEmitterServer.sendMessage(userId,tem);
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
+    public Mat getMatfromVideo(){
+        Mat img = new Mat();
+        //读取摄像头下的图像
+        if (!videoCapture.read(img)) return null;
+        return img;
+    }
+    public String matToBase64(Mat img){
+            if(img==null){
+                return null;
+            }
+            //图像缩放比例
+            double scale =4.0;
+            double fx=1/scale;
+
+            //初始化白名单人脸库，第一个参数为图片数量，第二个参数为图片存放地址
+            facecheck  face=new facecheck();
+            face.initFaceCheck(1,adminface);
+
+            Mat rgb = new Mat();
+            Imgproc.cvtColor(img, rgb, Imgproc.COLOR_BGR2RGB);
+            Mat gray = new Mat();
+            Imgproc.cvtColor(rgb, gray, Imgproc.COLOR_RGB2GRAY);
+
+            //图片缩放，减少计算量
+            Mat smallImg=new Mat();
+            Imgproc.resize(gray,smallImg,smallImg.size(),fx,fx,INTER_LINEAR);
+            //释放无用mat
+            gray.release();
+
+            //图片直方图均衡化
+            Imgproc.equalizeHist(smallImg,smallImg);
+
+
+
+            MatOfRect faveRect = new MatOfRect();
+            //检测人脸
+            faceDetector.detectMultiScale(gray, faveRect);
+            //图形面勾选人脸
+            for (Rect re : faveRect.toArray()) {
+
+                double lx=re.x*scale;
+                double ly=re.y*scale;
+                double lwidth=(re.x + re.width)*scale;
+                double lheigeht=(re.y + re.height)*scale;
+
+                //框出人脸
+                Imgproc.rectangle(img, new Point(lx, ly), new Point(lwidth, lheigeht), new Scalar(0, 255, 0), 2);
+                //创建人脸识别出的矩形变量
+                Mat imgcompare = new Mat(smallImg, new Rect(re.x, re.y, re.width, re.height));
+
+                //在白名单库中对比
+                boolean flag = face.faceCheck(imgcompare);
+                if (flag)
+                    Imgproc.putText(img, "admin", new Point(lx, ly), 2, 1, new Scalar(0, 255, 0));
+                else {
+                    Imgproc.rectangle(img, new Point(lx, ly), new Point(lwidth, lheigeht), new Scalar(0, 0, 255), 2);
+                    Imgproc.putText(img, "warn", new Point(lx, ly), 2, 1, new Scalar(0, 0, 255));
+//                    savePicture.savePicture(img);
                 }
             }
-        });
-        t.start();
-        return s;
+            String jpg_base64 = null;
+            Imgcodecs.imwrite(savepath + "tmp.jpg", img);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            try {
+                ImageIO.write(mat2BI(img), "jpg", baos);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            byte[] bytes = baos.toByteArray();
+//                    BASE64Encoder encoder = new BASE64Encoder();
+//                    jpg_base64 = encoder.encodeBuffer(Objects.requireNonNull(bytes));
+            Base64.Encoder encoder = Base64.getEncoder();
+            jpg_base64 = encoder.encodeToString(Objects.requireNonNull(bytes));
+            return jpg_base64;
+        }
+
+    public static BufferedImage mat2BI(Mat mat) {
+        int dataSize = mat.cols() * mat.rows() * (int) mat.elemSize();
+        byte[] data = new byte[dataSize];
+        mat.get(0, 0, data);
+        int type = mat.channels() == 1 ?
+                BufferedImage.TYPE_BYTE_GRAY : BufferedImage.TYPE_3BYTE_BGR;
+
+        if (type == BufferedImage.TYPE_3BYTE_BGR) {
+            for (int i = 0; i < dataSize; i += 3) {
+                byte blue = data[i + 0];
+                data[i + 0] = data[i + 2];
+                data[i + 2] = blue;
+            }
+        }
+        BufferedImage image = new BufferedImage(mat.cols(), mat.rows(), type);
+        image.getRaster().setDataElements(0, 0, mat.cols(), mat.rows(), data);
+
+        return image;
     }
 }
